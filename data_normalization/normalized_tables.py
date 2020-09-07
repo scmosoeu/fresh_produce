aa14eccafa7a53becd350a9228b1edcefe199882
Selebogo Mosoeu <45658709+scmosoeu@users.noreply.github.com>
2020-09-07 06:42:29 +0200

    created norm tables

--- data_normalization/normalized_tables.py
+++ data_normalization/normalized_tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from engine_info import server_info
 import urllib
@@ -10,7 +10,7 @@ class Product(Base):
     __tablename__ = 'product'
 
     id = Column(Integer, primary_key=True)
-    product_name = Column(String)
+    name = Column(String)
 
 
 class Container(Base):
@@ -18,5 +18,42 @@ class Container(Base):
     __tablename__ = 'container'
 
     id = Column(Integer, primary_key=True)
-    container_name = Column(String)
+    name = Column(String)
 
+
+class Inventory(Base):
+
+    __tablename__ = 'inventory'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    available = Column(Integer)
+
+class ProductCombo(Base):
+
+    __tablename__ = 'product_combination'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    name = Column(String)
+
+
+class Sales(Base):
+
+    __tablename__ = 'sales'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    # Some foreignkey
+    quantity_sold = Column(Integer)
+    cost = Column(Float)
+    average_price = Column(Float)
+    highest_price = Column(Float)
+
+
+if __name__ == "__main__":
+    
+    params = urllib.parse.quote_plus(server_info)
+    engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
+        
+    Base.metadata.create_all(engine)
