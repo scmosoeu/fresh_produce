39809afa6be3debd74bdf3c17b6a2d5b1efb9d21
Selebogo Mosoeu <45658709+scmosoeu@users.noreply.github.com>
2020-09-07 09:39:09 +0200

    updated date column

--- data_normalization/normalized_tables.py
+++ data_normalization/normalized_tables.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from engine_info import server_info
 import urllib
 
