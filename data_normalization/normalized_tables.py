ba288154d87248387f9ecc74aa1f480af7e57f96
Selebogo Mosoeu <45658709+scmosoeu@users.noreply.github.com>
2020-09-08 12:02:41 +0200

    updated tables

--- data_normalization/normalized_tables.py
+++ data_normalization/normalized_tables.py
@@ -1,69 +1,70 @@
-from sqlalchemy import create_engine, Column, Table, String, Integer, Float, ForeignKey, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from engine_info import server_info
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import urllib
 
-Base = declarative_base()
+app = Flask(__name__)
 
-class Product(Base):
+params = urllib.parse.quote_plus(server_info)
+database = f'mssql+pyodbc:///?odbc_connect={params}' 
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+db = SQLAlchemy(app)
+
+class Product(db.Model):
 
     __tablename__ = 'product'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False, unique=True)
-    product_sales = relationship('Sales', backref='product_sale')
-    product_inventories = relationship('Inventory', backref='product_inventory')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    product_sales = db.relationship('Sales', backref='product_sale')
+    product_inventories = db.elationship('Inventory', backref='product_inventory')
 
 
-class Container(Base):
+class Container(db.Model):
 
     __tablename__ = 'container'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False, unique=True)
-    container_sales = relationship('Sales', backref='container_sale')
-    container_inventories = relationship('Inventory', backref='container_inventory')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    container_sales = db.relationship('Sales', backref='container_sale')
+    container_inventories = db.relationship('Inventory', backref='container_inventory')
 
 
-class Inventory(Base):
+class Inventory(db.Model):
 
     __tablename__ = 'inventory'
 
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, nullable=False)
-    available = Column(Integer, nullable=False)
-    product_id = Column(Integer, ForeignKey('product.id'), nullable=False)
-    container_id = Column(Integer, ForeignKey('container.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    available = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    container_id = db.Column(db.Integer, db.ForeignKey('container.id'), nullable=False)
 
 
-class ProductCombination(Base):
+class ProductCombination(db.Model):
 
     __tablename__ = 'product_combination'
 
-    id = Column(Integer, primary_key=True)  
-    name = Column(String(200), nullable=False, unique=True)
-    combination_sales = relationship('Sales', backref='combination_sale')
+    id = db.Column(db.Integer, primary_key=True)  
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    combination_sales = db.relationship('Sales', backref='combination_sale')
 
 
-class Sales(Base):
+class Sales(db.Model):
 
     __tablename__ = 'sales'
 
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, nullable=False)
-    quantity_sold = Column(Integer, nullable=False)
-    kg_sold = Column(Float, nullable=False)
-    value = Column(Float, nullable=False)
-    average_price = Column(Float, nullable=False)
-    highest_price = Column(Float, nullable=False)
-    product_combination_id = Column(Integer, ForeignKey('product_combination.id'), nullable=False)
-    container_id = Column(Integer, ForeignKey('container.id'), nullable=False)
-    product_id = Column(Integer, ForeignKey('product.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    quantity_sold = db.Column(db.Integer, nullable=False)
+    kg_sold = db.Column(db.Float, nullable=False)
+    value = db.Column(db.Float, nullable=False)
+    average_price = db.Column(db.Float, nullable=False)
+    highest_price = db.Column(db.Float, nullable=False)
+    product_combination_id = db.Column(db.Integer, db.ForeignKey('product_combination.id'), nullable=False)
+    container_id = db.Column(db.Integer, db.ForeignKey('container.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 if __name__ == "__main__":
     
-    params = urllib.parse.quote_plus(server_info)
-    engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
-        
-    Base.metadata.create_all(engine)
+    db.create_all()
