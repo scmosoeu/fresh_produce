ee4d6768e65f90edc4c9dd8df8634a64f7c8340c
Armaan <armaan.maharaj@gmail.com>
2020-09-10 12:18:48 +0200

    only pipeline for normalising retailer data is not yet functional

--- data_normalization/retailers_normalized_data.py
+++ data_normalization/retailers_normalized_data.py
@@ -14,7 +14,7 @@ class RetailerProducts(db.Model):
 
     __tablename__ = 'RetailerProducts'
 
-    RetailerProductID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     RetailerProduct = db.Column(db.String(100), nullable=False, unique=True)
 
 
@@ -22,39 +22,39 @@ class Retailers(db.Model):
 
     __tablename__ = 'Retailers'
 
-    RetailerID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     RetailerName = db.Column(db.String(50), nullable=False, unique=True)
 
 class Date(db.Model):
 
     __tablename__ = 'Date'
 
-    DateID = db.Column(db.Integer, primary_key=True)
-    date_key = db.Column(db.DateTime, db.ForeignKey('master_date.date_key'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    date_key = db.Column(db.DateTime, nullable=False)#, db.ForeignKey('master_date.date_key'),)
 
 class RetailerPrices(db.Model):
 
     __tablename__ = 'RetailerPrices'
 
-    RetailerProductID = db.Column(db.Integer, db.ForeignKey('RetailerProducts.RetailerProductID'), nullable=False, primary_key=True)
+    RetailerProductID = db.Column(db.Integer, db.ForeignKey('RetailerProducts.id'), nullable=False, primary_key=True)
     Price = db.Column(db.Float, nullable=False)
-    DateID = db.Column(db.Integer, db.ForeignKey('Date.DateID'), nullable=False)
-    RetailerID = db.Column(db.Integer, db.ForeignKey("Retailes.RetailerID"))
+    DateID = db.Column(db.Integer, db.ForeignKey('Date.id'), nullable=False)
+    RetailerID = db.Column(db.Integer, db.ForeignKey("Retailers.id"))
 
-class MasterDate(db.Model):
+#class MasterDate(db.Model):
 
-    __tablename__ = 'master_date'
-    date_key = db.Column(db.DateTime, primary_key=True)
-    calendar_date = db.Column(db.String(20), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    half_of_year = db.Column(db.Integer, nullable=False)
-    quarter = db.Column(db.Integer, nullable=False)
-    month = db.Column(db.String(20), nullable=False)
-    week_of_year = db.Column(db.Integer, nullable=False)
-    day = db.Column(db.String(20), nullable=False)
+ #   __tablename__ = 'master_date'
+ #   date_key = db.Column(db.DateTime, primary_key=True)
+ #   calendar_date = db.Column(db.String(20), nullable=False)
+  ##  year = db.Column(db.Integer, nullable=False)
+  #  half_of_year = db.Column(db.Integer, nullable=False)
+  #  quarter = db.Column(db.Integer, nullable=False)
+  #  month = db.Column(db.String(20), nullable=False)
+  #  week_of_year = db.Column(db.Integer, nullable=False)
+  #  day = db.Column(db.String(20), nullable=False)
 
-    inventory_dates = db.relationship('Inventory', backref='inventory_date')
-    invoice_dates = db.relationship('Sales', backref='sales_date')
+  #  inventory_dates = db.relationship('Inventory', backref='inventory_date')
+   #s invoice_dates = db.relationship('Sales', backref='sales_date')
 
 if __name__ == "__main__":
     
