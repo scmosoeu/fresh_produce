8fe4faef83299205138357e2cba1650ee0185f7a
Selebogo Mosoeu <45658709+scmosoeu@users.noreply.github.com>
2020-10-11 22:01:12 +0200

    created ARIMA model

---
+++ forecasting/streamlit-app/models/arima_model.py
@@ -0,0 +1,26 @@
+from pmdarima import auto_arima
+
+def arima_forecast(df, col, num_of_periods):
+    """
+    Returns a trained ARIMA model
+
+    Parameters
+    -----------
+    df: DataFrame
+        A dataframe consisting of a column from which an ARIMA model 
+        will be developed
+    col: str
+        A column name from which an ARIMA model will be developed
+    num_of_periods: int
+        Number of periods ahead that the model will predict
+
+    Returns
+    --------
+    pd.Series
+        A series of predictions with the number of periods as the index
+    """
+
+    model = auto_arima(df[col], error_action='ignore', suppress_warnings=True)
+    pred = model.predict(n_periods=num_of_periods)
+
+    return pred  
