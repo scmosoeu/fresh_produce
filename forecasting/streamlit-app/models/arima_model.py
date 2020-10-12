8d124462d0257abb475850adf5cf7f008ae51873
Selebogo Mosoeu <45658709+scmosoeu@users.noreply.github.com>
2020-10-12 08:58:11 +0200

    added Holt-Winters

--- forecasting/streamlit-app/models/arima_model.py
+++ forecasting/streamlit-app/models/arima_model.py
@@ -1,9 +1,9 @@
 from pmdarima import auto_arima
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
-def arima_forecast(df, col, num_of_periods, method):
+def arima_forecast(df, col, num_of_periods):
     """
-    Returns a trained ARIMA model
+    Returns predictions based on ARIMA model
 
     Parameters
     -----------
@@ -14,8 +14,32 @@ def arima_forecast(df, col, num_of_periods, method):
         A column name from which an ARIMA model will be developed
     num_of_periods: int
         Number of periods ahead that the model will predict
-    method: str
-        A forecasting method used in generating predictions
+
+    Returns
+    --------
+    pd.Series
+        A series of predictions with the number of periods as the index
+    """
+    
+    model = auto_arima(df[col], error_action='ignore', suppress_warnings=True)
+    pred = model.predict(n_periods=num_of_periods)
+
+    return pred  
+
+
+def holt_winters_forecast(df, col, num_of_periods):
+    """
+    Returns predictions based on Holt-Winters method
+
+    Parameters
+    -----------
+    df: DataFrame
+        A dataframe consisting of a column from which an ARIMA model 
+        will be developed
+    col: str
+        A column name from which an ARIMA model will be developed
+    num_of_periods: int
+        Number of periods ahead that the model will predict
 
     Returns
     --------
@@ -23,11 +47,14 @@ def arima_forecast(df, col, num_of_periods, method):
         A series of predictions with the number of periods as the index
     """
 
-    model = auto_arima(df[col], error_action='ignore', suppress_warnings=True)
-    pred = model.predict(n_periods=num_of_periods)
+    model = ExponentialSmoothing(
+        endog=df[col],
+        trend='add',
+        seasonal='add',
+        seasonal_periods=7
+    )
 
-    return pred  
+    fitted_model = model.fit()
+    pred = fitted_model.forecast(steps=num_of_periods)
 
-
-def holt_winters(df, col, num_of_periods):
-
+    return pred 
